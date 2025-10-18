e05c4d23209e42c98ba5c75128880c4636fe3e03
chakradhar <chakradhar43@gmail.com>
2025-10-18 14:05:26 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req,res){
     res.send('Welcome to our Hotel');
@@ -15,7 +17,9 @@ app.use('/person',personRoutes);
 const menuItem = require('./routes/menuItemRoutes');
 app.use('/menuItem',menuItem);
 
-app.listen(3100,()=>{
+
+
+app.listen(PORT,()=>{
     console.log('listening on port 3100');
 })
 
